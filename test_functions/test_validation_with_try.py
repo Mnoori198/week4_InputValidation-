2e740de37b20d3424a89d2a632b43f0680c32b8c
mqasi <mnoori@dmacc.edu>
2019-09-21 18:10:39 -0500

    third test failed.

--- test_functions/test_validation_with_try.py
+++ test_functions/test_validation_with_try.py
@@ -5,7 +5,7 @@ from main_function import validation_with_try
 
 class MyTestCase(unittest.TestCase):
     def test_average_score(self):
-        with mock.patch('builtins.input', side_effect=(-90, 95,85)):
+        with mock.patch('builtins.input', side_effect=(-90, -95,85)):
             self.assertRaises(ValueError)
 
 
